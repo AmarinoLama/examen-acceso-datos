3bd2bf216defbe205d5a581d832fb70bc609cd8b
AmarinoLama <amarinolama@gmail.com>
2024-05-27 10:11:51 +0200

    feat (Repositorio) : add method loadItems

--- src/main/java/org/acme/Repositorio.java
+++ src/main/java/org/acme/Repositorio.java
@@ -23,4 +23,22 @@ public class Repositorio {
                                                     .toList();
         return !listaFiltrada.isEmpty() ? MagicalItem.findByIdOptional(listaFiltrada.get(0).getId()) : Optional.empty();
     }
+
+    /*public Optional<MagicalItem> loadItem(MagicalItem item) {
+        List<MagicalItem> listaItems = MagicalItem.listAll();
+        List<MagicalItem> listaFiltrada = new ArrayList<>();
+        for (MagicalItem newItem : listaItems) {
+            if (newItem.getName() == item.getName() && newItem.getQuality() == item.getQuality() && newItem.getTipoItem() == item.getTipoItem()) {
+                listaFiltrada.add(newItem);
+            }
+        }
+        return !listaFiltrada.isEmpty() ? MagicalItem.findByIdOptional(listaFiltrada.get(0).getId()) : Optional.empty();
+    }*/
+
+    public List<MagicalItem> loadItems(String nombreItem) {
+        List<MagicalItem> listaItems = MagicalItem.listAll();
+        return listaItems.stream()
+                .filter(item -> item.getName().equals(nombreItem))
+                .toList();
+    }
 }
