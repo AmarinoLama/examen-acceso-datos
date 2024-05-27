876ee15000e667d6ca2756f09f5992bcc62b1b89
AmarinoLama <amarinolama@gmail.com>
2024-05-27 09:58:22 +0200

    feat (loadItem) : add method loadItem

--- src/main/java/org/acme/Repositorio.java
+++ src/main/java/org/acme/Repositorio.java
@@ -1,8 +1,11 @@
 package org.acme;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.acme.domain.MagicalItem;
 import org.acme.domain.Wizard;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
@@ -11,5 +14,13 @@ public class Repositorio {
     public Optional<Wizard> loadWizard(String nombreWizard) {
         Optional<Wizard> wizard = Wizard.findByIdOptional(nombreWizard);
         return wizard;
+    }
+
+    public Optional<MagicalItem> loadItem(String nombreItem) {
+        List<MagicalItem> listaItems = MagicalItem.listAll();
+        List<MagicalItem> listaFiltrada = listaItems.stream()
+                                                    .filter(item -> item.getName().equals(nombreItem))
+                                                    .toList();
+        return !listaFiltrada.isEmpty() ? MagicalItem.findByIdOptional(listaFiltrada.get(0).getId()) : Optional.empty();
     }
 }
