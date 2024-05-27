1ab8d3ce5e90607942a31a31db7b407dc7f19835
AmarinoLama <amarinolama@gmail.com>
2024-05-27 08:43:34 +0200

    feat (MagicalItem) : add attributes

--- src/main/java/org/acme/domain/MagicalItem.java
+++ src/main/java/org/acme/domain/MagicalItem.java
@@ -1,4 +1,21 @@
 package org.acme.domain;
 
-public class MagicalItem {
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+
+@Entity
+@Table (name="t_items")
+public class MagicalItem extends PanacheEntityBase {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="item_id")
+    public long idItem = 0L;
+
+    @Column(name="item_name")
+    public String nombreItem = "";
+
+    @Column(name="item_quality")
+    public int calidadItem = 0;
+
+    @Column(name="item_type")
+    public String tipoItem = "";
 }
