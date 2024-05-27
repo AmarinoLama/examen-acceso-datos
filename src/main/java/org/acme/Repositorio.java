cacd9fe3a91eaedad8cbbc7c95f424fdf96a05de
AmarinoLama <amarinolama@gmail.com>
2024-05-27 09:26:54 +0200

    feat (Repositorio) : add method load_wizard

--- src/main/java/org/acme/Repositorio.java
+++ src/main/java/org/acme/Repositorio.java
@@ -1,7 +1,15 @@
 package org.acme;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.acme.domain.Wizard;
+
+import java.util.Optional;
+import java.util.function.Supplier;
 
 @ApplicationScoped
 public class Repositorio {
+    public Optional<Wizard> loadWizard(String nombreWizard) {
+        Optional<Wizard> wizard = Wizard.findByIdOptional(nombreWizard);
+        return wizard;
+    }
 }
